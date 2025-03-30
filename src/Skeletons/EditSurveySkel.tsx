b856001b07e3d684bdda8ecd01b49edbbcdacb5b
amarnath vishwakarma <52070562+amarsharmamrj@users.noreply.github.com>
2025-03-31 00:14:46 +0530

    Update EditSurveySkel.tsx

--- src/Skeletons/EditSurveySkel.tsx
+++ src/Skeletons/EditSurveySkel.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid, Skeleton } from "@mui/material"
   
-const EditSurveySkel = (props: any) => { 
+const EditSurveySkel = (props: any) => {  
 
     return (
         <Grid item xs={12} sm={12} md={10}>   
