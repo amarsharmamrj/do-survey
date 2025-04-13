03f096d8ea09619f8e77cb325155cb5b4a928f25
amarnath vishwakarma <52070562+amarsharmamrj@users.noreply.github.com>
2025-04-13 09:07:36 +0530

    Update Actions.tsx

--- src/components/CreateSurvey/Actions.tsx
+++ src/components/CreateSurvey/Actions.tsx
@@ -1,6 +1,6 @@
 import { Box, Divider, IconButton, Switch, Tooltip } from "@mui/material" 
 import AddCircleIcon from '@mui/icons-material/AddCircle'; 
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from '@mui/icons-material/Delete'; 
 
 const Actions = (props:any) => {
     const {question, handleRequired, handleDeleteQuestion, handleAddQuestion } = props  
