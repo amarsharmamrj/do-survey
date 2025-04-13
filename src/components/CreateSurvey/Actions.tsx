f786305e705c500c768e1c5b54c72d9d1174e410
amarnath vishwakarma <52070562+amarsharmamrj@users.noreply.github.com>
2025-04-13 09:07:14 +0530

    Update Actions.tsx

--- src/components/CreateSurvey/Actions.tsx
+++ src/components/CreateSurvey/Actions.tsx
@@ -1,5 +1,5 @@
 import { Box, Divider, IconButton, Switch, Tooltip } from "@mui/material"
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle'; 
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const Actions = (props:any) => {
