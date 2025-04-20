49e7f1d7abbcb16e853128108566f86f557d3691
amarnath vishwakarma <52070562+amarsharmamrj@users.noreply.github.com>
2025-04-20 18:44:48 +0530

    Update EditSurveySkel.tsx

--- src/Skeletons/EditSurveySkel.tsx
+++ src/Skeletons/EditSurveySkel.tsx
@@ -4,7 +4,7 @@ const EditSurveySkel = (props: any) => {
  
     return ( 
         <Grid item xs={12} sm={12} md={10}>      
-            <Box className="question-box">  
+            <Box className="question-box">   
   
                 <Skeleton variant="rounded" sx={{ width: '100%', height: '3.2rem', margin: '1rem 0' }} />  
                 <Skeleton variant="rounded" sx={{ width: '100%', height: '3.2rem', margin: '1rem 0' }} /> 
